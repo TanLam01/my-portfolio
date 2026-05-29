6228374a4622760fa05204ce43305b916f9bc972
Lam_Tat@v.dev <nesv061@gmail.com>
2026-05-29 08:55:15 +0700

    modify code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,8 +20,8 @@ export default function Home() {
             <main>
                 <HomeSection />
                 <AboutSection />
-                <SkillSection />
                 <ProjectsSection />
+                <SkillSection />
                 <ContactSection />
             </main>
 
